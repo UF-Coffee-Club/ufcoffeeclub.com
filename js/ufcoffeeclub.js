0f833b401913b0c2374ecc4577cc9c18b70534ee
Paul Le Grand <paul@paullegrand.org>
2018-01-16 17:10:30 -0500

    Test to see if all of sourcetree is broken or just sm repo

---
+++ js/ufcoffeeclub.js
@@ -0,0 +1,8 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Initialize and Configure Scroll Reveal Animation
+    window.sr = ScrollReveal();
+    sr.reveal('.reveal');
+
+}); // End of use strict
